431483913ca6992af049f085b5cebfbb451abaa2
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2021-06-01 17:33:34 -0300

    Finalizando sipa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 import Head from 'next/head';
-import Prismic from '@prismicio/client'
-
 import { FiCalendar, FiUser } from 'react-icons/fi';
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
+import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
 interface Post {
@@ -24,56 +29,122 @@ interface PostPagination {
 }
 
 interface HomeProps {
+  preview: boolean;
   postsPagination: PostPagination;
 }
 
- export default function Home() {
-   return (
-     <>
+export default function Home({
+  preview,
+  postsPagination,
+}: HomeProps): JSX.Element {
+  const [posts, setPosts] = useState<PostPagination>({
+    ...postsPagination,
+    results: postsPagination.results.map(post => ({
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    })),
+  });
+
+  async function loadMorePosts(): Promise<void> {
+    const response = await fetch(`${posts.next_page}`).then(data =>
+      data.json()
+    );
+
+    const postsResponseResults = response.results.map(post => ({
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    }));
+
+    const newPosts = {
+      ...posts,
+      next_page: response.next_page,
+      results: [...posts.results, ...postsResponseResults],
+    };
+
+    setPosts(newPosts);
+  }
+
+  return (
+    <>
       <Head>
-        <title>Posts | Spacetraveling</title>
+        <title>Home | SpaceTraveling</title>
       </Head>
 
       <main className={styles.container}>
-        <div className={styles.posts}>
-          <a href="#">
-            <strong>Creating a monorepo</strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adip</p>
-            <span><FiCalendar /> Data</span>  &nbsp;&nbsp;&nbsp;&nbsp;
-            <span><FiUser /> Autor</span>
-          </a>
-          <a href="#">
-            <strong>Creating a monorepo</strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adip</p>
-            <span><FiCalendar/> Data</span>  &nbsp;&nbsp;&nbsp;&nbsp;
-            <span><FiUser /> Autor</span>
-          </a>
-          <a href="#">
-            <strong>Creating a monorepo</strong>
-            <p>Lorem ipsum dolor sit amet, consectetur adip</p>
-            <span><FiCalendar/> Data</span>  &nbsp;&nbsp;&nbsp;&nbsp;
-            <span><FiUser /> Autor</span>
-          </a>
-         <button>
-           Carregar mais posts
-         </button>
-        </div>
+        <section>
+          {posts.results.map(post => (
+            <article className={styles.postContent} key={post.uid}>
+              <Link href={`/post/${post.uid}`}>
+                <a>
+                  <h1>{post.data.title}</h1>
+                  <p>{post.data.subtitle}</p>
+                  <div>
+                    <time>
+                      <FiCalendar />
+                      {post.first_publication_date}
+                    </time>
+                    <span>
+                      <FiUser />
+                      {post.data.author}
+                    </span>
+                  </div>
+                </a>
+              </Link>
+            </article>
+          ))}
+          {posts.next_page && (
+            <button type="button" onClick={loadMorePosts}>
+              Carregar mais posts
+            </button>
+          )}
+          {preview && (
+            <aside className={commonStyles.exitPreviewButton}>
+              <Link href="/api/exit-preview">
+                <a>Sair do modo Preview</a>
+              </Link>
+            </aside>
+          )}
+        </section>
       </main>
     </>
-   )
- }
+  );
+}
 
- export const getStaticProps: GetStaticProps = async () => {
-  const prismic = getPrismicClient()
+export const getStaticProps: GetStaticProps = async ({
+  preview = false,
+  previewData,
+}) => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      pageSize: 2, // posts per page
+      ref: previewData?.ref ?? null,
+    }
+  );
 
-  const postsResponse = await prismic.query([
-    Prismic.Predicates.at('document.type', 'posts')
-  ], {
-    fetch: ['posts.title', 'posts.content'],
-    pageSize: 100,
-  })
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResponse.results,
+  };
 
   return {
-    props: {}
+    props: {
+      postsPagination,
+      preview,
+    },
+    revalidate: 60 * 5, // 5 minutes
   };
 };
